b0787e4a8c73d833d3e3cdb7f77fc336894ed74a
mubashir-bashir <mb.mubashirbashir@gmail.com>
2024-03-04 16:02:01 +0500

    Day 02

---
+++ Day_02/07_NumberEight.js
@@ -0,0 +1,9 @@
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+// Addition: 5 + 3
+console.log(5 + 3); // Output: 8
+// Subtraction: 10 - 2
+console.log(10 - 2); // Output: 8
+// Multiplication: 4 * 2
+console.log(4 * 2); // Output: 8
+// Division: 16 / 2
+console.log(16 / 2); // Output: 8
