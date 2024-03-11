145f43ec97a51794c07446fe5b6cb28a945ab867
mubashir-bashir <mb.mubashirbashir@gmail.com>
2024-03-11 18:28:58 +0500

    Day-09

---
+++ Day-09/29.Favourite_Fruit.js
@@ -0,0 +1,19 @@
+// Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+// • Make a array of your three favorite fruits and call it favorite_fruits.
+// • Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+var fav_fruit = ['Banana', "Orange", 'Strawberry'];
+if (fav_fruit.includes("Graps")) {
+    console.log("You Really like Banana");
+}
+if (fav_fruit.includes("Banana")) {
+    console.log("You really like Banana");
+}
+if (fav_fruit.includes("Mange")) {
+    console.log("You really like Mange");
+}
+if (fav_fruit.includes("Strawberry")) {
+    console.log("You really like Strawberry");
+}
+if (fav_fruit.includes("Orange")) {
+    console.log("You really like Orange");
+}
---
+++ Day-09/30.Admin.ts
@@ -0,0 +1,15 @@
+// Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
+// • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+// • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
+
+let username = ['Mubashir','Ali', 'Adnan',"Admin","kamran"]
+
+
+for(const name of username){
+    if(name ==='Admin'){
+        console.log("Hello Admin, would you like to see a status report?")
+    }
+    else{
+        console.log(`Hello ${name} thank you for loggin in again`)
+    }
+}
