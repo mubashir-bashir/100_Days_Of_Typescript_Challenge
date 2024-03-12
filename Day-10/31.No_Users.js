90599e449331c05d426fda9b2c9f56082a511cd6
mubashir-bashir <mb.mubashirbashir@gmail.com>
2024-03-12 18:34:10 +0500

    Day-10

---
+++ Day-10/31.No_Users.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var usernames = ['Ali', "Adnan", "Kamran", "Mubashir", "Hammad"];
+if (usernames.length == 0) {
+    console.log("We need to find some users!");
+}
+else {
+    usernames = [];
+    console.log("All user name removed");
+}
---
+++ Day-10/31.No_Users.ts
@@ -0,0 +1,13 @@
+// No Users: Add an if test to  make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
+// • Remove all of the usernames from your array, and make sure the correct message is printed.
+export{}
+let usernames:string[]= ['Ali',"Adnan","Kamran","Mubashir","Hammad"]
+
+if(usernames.length==0){
+    console.log("We need to find some users!")
+}
+else{
+    usernames= []
+    console.log("All user name removed")
+}
