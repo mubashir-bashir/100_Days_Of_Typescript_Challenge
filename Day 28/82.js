7bf5bce565aeae768d2d571dbcf6e408b6893b83
Mubashir Bashir <138807506+mubashir-bashir@users.noreply.github.com>
2024-03-29 22:16:00 +0500

    Delete Day 28 directory

--- Day 28/82.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function countCharacters(str) {
-    return str.length;
-}
-var name = "Mubashir";
-console.log("Length of the string:", countCharacters(name)); // Output: 13
