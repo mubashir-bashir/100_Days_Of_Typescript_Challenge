c245baf9bfdeb03e0c3c608311ec908c58e14b78
mubashir-bashir <mb.mubashirbashir@gmail.com>
2024-03-31 23:17:13 +0500

    Day-30

---
+++ Day-30/88.js
@@ -0,0 +1,5 @@
+// Rounding to the Nearest Integer: Write a function that takes a decimal number as input and returns the number rounded to the nearest integer.
+function round(num) {
+    console.log(Math.round(num));
+}
+round(3310.766);
