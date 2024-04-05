5841d12b9ec118ad732d234d02bb3ec7e9fee863
mubashir-bashir <mb.mubashirbashir@gmail.com>
2024-04-05 18:25:21 +0500

    Day-35

--- Day-30/89.ts
+++ Day-30/89.ts
@@ -1,5 +1,4 @@
 // Converting Strings to Numbers: Create a function that takes a string representing a number (like "123") and converts it into an actual number type.
-
 // This function changes a string into a number
 function convertStringToNumber(str: string): number {
     return parseFloat(str); // Converts the string to a number
